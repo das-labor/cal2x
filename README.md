f1c18871c846cd51853898b8d0d1805dd531aced
TheRadialActive <account@d2ns.org>
2019-11-09 20:25:53 +0100

    python2->python3, README updated

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
 # cal2x
 Scripts for parsing and using iCal feed of das-labor
 
-=== cal2sound ===
+## cal2sound
 
 Get iCal feed and send a "ping" sound to the local pulseaudio server to stream it to all rooms.
 
-Install icalendar python module with pip
-	# pip2 install icalendar
+Install icalendar python module with pip:
 
-Install dependencies
-	# apt-get install alsa-base pulseaudio mplayer
+    # pip install icalendar
 
-Add this line to crontab
+Install dependencies (debian, ubuntu, linuxmint):
 
-	* * * * * /usr/bin/python2 /home/pi/cal2sound.py
+	# apt-get install alsa-base pulseaudio sox
 
+
+Add this line to crontab:
+
+	* * * * * /usr/bin/python /home/pi/cal2sound.py
+
+
+### Pulseaudio Streaming Server
+
+If you want to stream to a pulseaudio server, this script is for the client.
+To create a host install the graphical tool "papref" and select the checkbox of
+"Enable network access to local sound devices" and "Don't require
+authentication".
+
+Then initiate the sink on the client (where this script lives) via:
+    # pactl load-module module-tunnel-sink-new server=<ip-of-host> sink_name=<your sink name> channels=2 rate=32000
+
+Then select the sink (tunneling to the host) as audio output.
+
+### Thanks
+
+to cdrk for the [Reception bell sound](https://freesound.org/people/cdrk/sounds/264594/ )
+licensed under [CC-BY-3.0](http://creativecommons.org/licenses/by/3.0/)
--- cal2sound.py
+++ cal2sound.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+
 import icalendar
-import urllib2
+import urllib.request
 import datetime
 import os
 import pickle
@@ -8,40 +9,48 @@ import pickle
 calfile = "/tmp/laborcalendar.pickle"
 datafile = "/tmp/laborcal_data"
 
+
 def now_in_5min_timerange(point_in_time, now):
-        min5_ago = point_in_time - datetime.timedelta(minutes=5)
-        return min5_ago < now < point_in_time
+    min5_ago = point_in_time - datetime.timedelta(minutes=5)
+    return min5_ago < now < point_in_time
+
 
 def cal2sound():
-        req = urllib2.urlopen('https://www.das-labor.org/termine.ics')
-        data = req.read()
-        data2 = None
+    req = urllib.request.urlopen('https://www.das-labor.org/termine.ics')
+    data = req.read()
+    data2 = None
 
-        if os.path.exists(datafile):
-                with open(datafile, "rb") as fd:
-                        data2 = pickle.load(fd)
+    if os.path.exists(datafile):
+        with open(datafile, "rb") as fd:
+            data2 = pickle.load(fd)
 
-        now = datetime.datetime.now()
-#        now = datetime.datetime(year=2015, month=11, day=26, hour=19, minute=59, second=59) #Test
+    now = datetime.datetime.now()
 
-        cal = None
-        """parse calendar only of ics file differs, else load from pickle file"""
-        if not data == data2:
-                cal = icalendar.Calendar.from_ical(data)
-                with open(datafile, "wb", pickle.HIGHEST_PROTOCOL) as fd:
-                        pickle.dump(data, fd)
-                with open(calfile, "wb", pickle.HIGHEST_PROTOCOL) as fd:
-                        pickle.dump(cal, fd)
-        else:
-                with open(calfile, "rb") as fd:
-                        cal = pickle.load(fd)
+    cal = None
+    """parse calendar only of ics file differs, else load from pickle file"""
+    if not data == data2:
+        cal = icalendar.Calendar.from_ical(data)
+        with open(datafile, "wb", pickle.HIGHEST_PROTOCOL) as fd:
+            pickle.dump(data, fd)
+        with open(calfile, "wb", pickle.HIGHEST_PROTOCOL) as fd:
+            pickle.dump(cal, fd)
+    else:
+        with open(calfile, "rb") as fd:
+            cal = pickle.load(fd)
 
-        for event in cal.walk('vevent'):
-                nextevent_date = str(event.get('dtstart').dt)
-                parsed_datetime = datetime.datetime.strptime(nextevent_date, '%Y-%m-%d %H:%M:%S')
-                if now_in_5min_timerange(parsed_datetime, now):
-                        print("TODAYS is an event")
-                        os.system("mplayer -ao pulse:10.0.1.6 /home/pi/bell.flac ")
-                        break
+    for event in cal.walk('vevent'):
+        parsed_datetime = ''
+        nextevent_date = str(event.get('dtstart').dt)
+        try:
+            parsed_datetime = datetime.datetime.strptime(nextevent_date,
+                                                         '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            parsed_datetime = ''
+
+        if parsed_datetime and now_in_5min_timerange(parsed_datetime, now):
+            print("TODAY is an event")
+            os.system("play 264594__cdrk__reception-bell.flac")
+            break
+
 
 cal2sound()
